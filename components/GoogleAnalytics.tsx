ce02d5c2ed38449a83ccf1a6566caa378be25f66
satorukomaba <satoru.komaba@gmail.com>
2025-06-08 11:22:13 +0900

    アナリティクス追加

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import Script from 'next/script'
 
-const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-ZSD6BEE4YL'
 
 const GoogleAnalytics = () => {
   // 本番環境でのみGoogle Analyticsを実行
-  if (!GA_TRACKING_ID || process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== 'production') {
     return null
   }
 
   return (
     <>
+      {/* Google tag (gtag.js) */}
       <Script
-        strategy="afterInteractive"
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
       />
       <Script
         id="google-analytics"
@@ -23,10 +25,8 @@ const GoogleAnalytics = () => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_title: document.title,
-              page_location: window.location.href,
-            });
+            
+            gtag('config', '${GA_TRACKING_ID}');
           `,
         }}
       />
